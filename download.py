7b3a3fac128ae7a6e239f3f6fdb29a69f04684eb
pschlo <70105856+pschlo@users.noreply.github.com>
2024-06-13 08:43:26 +0200

    updated download.py

--- download.py
+++ download.py
@@ -6,6 +6,7 @@ import subprocess
 import stat
 from tarfile import TarFile
 from gzip import GzipFile
+import sys
 
 
 BW_CLI = "2024.4.1"
@@ -18,17 +19,27 @@ SCRIPT = "run-bw-backup"
 
 BW_DATA_DIR.mkdir()
 
-print(f"Downloading Bitwarden CLI")
-r = requests.get(rf'https://github.com/bitwarden/clients/releases/download/cli-v{BW_CLI}/bw-linux-{BW_CLI}.zip')
-r.raise_for_status()
-ZipFile(BytesIO(r.content)).extractall(BW_DATA_DIR)
+# print(f"Downloading Bitwarden CLI")
+# r = requests.get(rf'https://github.com/bitwarden/clients/releases/download/cli-v{BW_CLI}/bw-linux-{BW_CLI}.zip')
+# r.raise_for_status()
+# ZipFile(BytesIO(r.content)).extractall(BW_DATA_DIR)
 
 print(f"Downloading pywarden")
 r = requests.get(rf'https://github.com/pschlo/pywarden/releases/download/v{PYWARDEN}/pywarden-{PYWARDEN}.tar.gz')
 r.raise_for_status()
-TarFile(fileobj=GzipFile(fileobj=BytesIO(r.content))).extractall(BW_DATA_DIR)
+f = TarFile(fileobj=GzipFile(fileobj=BytesIO(r.content)))
 
-print(f"Downloading bw-backup")
-r = requests.get(rf'https://github.com/pschlo/bw-backup/releases/download/v{BW_BACKUP}/bw_backup-{BW_BACKUP}.tar.gz')
-r.raise_for_status()
-TarFile(fileobj=GzipFile(fileobj=BytesIO(r.content))).extractall(BW_DATA_DIR)
+prefix = f'pywarden-{PYWARDEN}/src/pywarden'
+members = [m for m in f.getmembers() if m.name.startswith(prefix)]
+for m in members:
+    m.name = m.name.removeprefix(prefix).removeprefix('/')
+f.extractall(BW_DATA_DIR / 'pywarden', members=members)
+
+
+# print(f"Downloading bw-backup")
+# r = requests.get(rf'https://github.com/pschlo/bw-backup/releases/download/v{BW_BACKUP}/bw_backup-{BW_BACKUP}.tar.gz')
+# r.raise_for_status()
+# TarFile(fileobj=GzipFile(fileobj=BytesIO(r.content))).extractall(BW_DATA_DIR)
+
+
+sys.executable
